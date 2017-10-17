a02b4ec36c3e276530dd1111842aa749451c5089
Rafaël Warnault <rw@opale.pro>
2017-10-17 12:12:25 +0200

    Initial Commit

---
+++ DICOM Test/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DICOM Test
+//
+//  Created by Rafael Warnault on 17/10/2017.
+//  Copyright © 2017 Read-Write.fr. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
