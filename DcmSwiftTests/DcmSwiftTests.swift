e9eef2d56f07beb7bce7633e67692c8528c2275d
Poulpy <paul.repain@yahoo.com>
2019-05-14 15:13:11 +0200

    Tests fixed for description of date (local problem)

--- DcmSwiftTests/DcmSwiftTests.swift
+++ DcmSwiftTests/DcmSwiftTests.swift
@@ -56,6 +56,10 @@ class DcmSwiftTests: XCTestCase {
         if testDicomDateAndTime {
             suite.addTest(DcmSwiftTests(selector: #selector(readDicomDate)))
             suite.addTest(DcmSwiftTests(selector: #selector(writeDicomDate)))
+            suite.addTest(DcmSwiftTests(selector: #selector(dicomDateWrongLength)))
+            suite.addTest(DcmSwiftTests(selector: #selector(readDicomTimeMidnight)))
+            suite.addTest(DcmSwiftTests(selector: #selector(dicomTimeWrongLength)))
+            suite.addTest(DcmSwiftTests(selector: #selector(dicomTimeWeirdTime)))
             suite.addTest(DcmSwiftTests(selector: #selector(readDicomTime)))
             suite.addTest(DcmSwiftTests(selector: #selector(writeDicomTime)))
             suite.addTest(DcmSwiftTests(selector: #selector(combineDateAndTime)))
@@ -120,18 +124,18 @@ class DcmSwiftTests: XCTestCase {
     public func readDicomDate() {
         let ds1 = "20001201"
         let dd1 = Date(dicomDate: ds1)
-        let desc1 = dd1!.description(with: .current)
-        let r1 = "vendredi 1 décembre 2000 à 00:00:00 heure normale d’Europe centrale"
-        
-        assert(desc1 == r1)
+
+        let df = DateFormatter()
+        df.dateFormat = "yyyy/MM/dd HH:mm:ss"
+        let expected_res = "2000/12/01 00:00:00"
+
+        assert(expected_res == df.string(from: dd1!))
         
         // ACR-NEMA date format
         let ds2 = "2000.12.01"
         let dd2 = Date(dicomDate: ds2)
-        let desc2 = dd2!.description(with: .current)
-        let r2 = "vendredi 1 décembre 2000 à 00:00:00 heure normale d’Europe centrale"
-        
-        assert(desc2 == r2)
+
+        assert(expected_res == df.string(from: dd2!))
     }
     
     
@@ -164,20 +168,22 @@ class DcmSwiftTests: XCTestCase {
     
     
     public func readDicomTime() {
+        let df = DateFormatter()
+        df.dateFormat = "yyyy/MM/dd HH:mm:ss"
+        let expected_res = "2000/01/01 14:32:50"
+
         let ds1 = "143250"
         let dd1 = Date(dicomTime: ds1)
-        let desc1 = dd1!.description(with: .current)
-        let r1 = "samedi 1 janvier 2000 à 14:32:50 heure normale d’Europe centrale"
-        
-        assert(desc1 == r1)
-        
+
+        assert(expected_res == df.string(from: dd1!))
+
+
+
         // ACR-NEMA time format
         let ds2 = "14:32:50"
         let dd2 = Date(dicomTime: ds2)
-        let desc2 = dd2?.description(with: .current)
-        let r2 = "samedi 1 janvier 2000 à 14:32:50 heure normale d’Europe centrale"
-        
-        assert(desc2 == r2)
+
+        assert(expected_res == df.string(from: dd2!))
     }
 
 
@@ -236,13 +242,16 @@ class DcmSwiftTests: XCTestCase {
 
     
     public func combineDateAndTime() {
+        let df = DateFormatter()
+        df.dateFormat = "yyyy/MM/dd HH:mm:ss"
+        let expected_res = "2000/12/01 14:32:50"
+
         let ds1 = "20001201"
         let ts1 = "143250"
         
         let dateAndTime = Date(dicomDate: ds1, dicomTime: ts1)
-        let dts = dateAndTime?.description(with: .current)
-        
-        assert(dts == "vendredi 1 décembre 2000 à 14:32:50 heure normale d’Europe centrale")
+
+        assert(expected_res == df.string(from: dateAndTime!))
     }
     
     
