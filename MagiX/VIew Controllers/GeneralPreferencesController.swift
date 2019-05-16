70f24dffffe13ce9a86b251ed5d75669948728f5
Poulpy <paul.repain@yahoo.com>
2019-05-16 16:11:01 +0200

    Logger preferences panel

---
+++ MagiX/VIew Controllers/GeneralPreferencesController.swift
@@ -0,0 +1,18 @@
+//
+//  GeneralPreferencesController.swift
+//  MagiX
+//
+//  Created by paul on 16/05/2019.
+//  Copyright © 2019 Read-Write.fr. All rights reserved.
+//
+
+import Cocoa
+
+class GeneralPreferencesController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
