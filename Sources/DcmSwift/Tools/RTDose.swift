b849365bdab6ecc7046c1bb18f3fb18327996322
Paul <pr@opale.pro>
2021-07-07 09:58:46 +0200

    Init RTDose class, with dcmtk as template : https://support.dcmtk.org/docs/classDRTDose.html#aab575ebb9e190a17ae45c1a9abb9ee68

---
+++ Sources/DcmSwift/Tools/RTDose.swift
@@ -0,0 +1,34 @@
+//
+//  File.swift
+//  
+//
+//  Created by Paul on 07/07/2021.
+//
+
+import Foundation
+
+public class RTDose : DicomFile {
+    public func getDose() {
+        
+    }
+    
+    public func getUnscaledDose() {
+        
+    }
+    
+    public func getDoseImage() {
+        
+    }
+    
+    public func getDoseImageWidth() {
+        
+    }
+    
+    public func getDoseImageHeight() {
+        
+    }
+    
+    public func isValid() {
+        
+    }
+}
